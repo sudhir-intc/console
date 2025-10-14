77389665fd1efcd07683df00c98510f8ca394d8e
NRO\r.i.preda <robert.preda@netrom.ro>
2025-10-14 10:42:22 +0300

    feat: include translation files as static assets

--- internal/controller/http/router.go
+++ internal/controller/http/router.go
@@ -66,7 +66,6 @@ func NewRouter(handler *gin.Engine, l logger.Interface, t usecase.Usecases, cfg 
 	handler.StaticFileFS("/vendor.js", "./vendor.js", http.FS(staticFiles))
 	handler.StaticFileFS("/favicon.ico", "./favicon.ico", http.FS(staticFiles))
 	handler.StaticFileFS("/assets/logo.png", "./assets/logo.png", http.FS(staticFiles))
-	handler.StaticFileFS("/assets/i18n/en.json", "./assets/i18n/en.json", http.FS(staticFiles))
 	handler.StaticFileFS("/assets/monaco/min/vs/loader.js", "./assets/monaco/min/vs/loader.js", http.FS(staticFiles))
 	handler.StaticFileFS("/assets/monaco/min/vs/editor/editor.main.js", "./assets/monaco/min/vs/editor/editor.main.js", http.FS(staticFiles))
 	handler.StaticFileFS("/assets/monaco/min/vs/editor/editor.main.css", "./assets/monaco/min/vs/editor/editor.main.css", http.FS(staticFiles))
@@ -75,6 +74,13 @@ func NewRouter(handler *gin.Engine, l logger.Interface, t usecase.Usecases, cfg 
 	handler.StaticFileFS("/assets/monaco/min/vs/base/common/worker/simpleWorker.nls.js", "./assets/monaco/min/vs/base/common/worker/simpleWorker.nls.js", http.FS(staticFiles))
 	handler.StaticFileFS("/assets/monaco/min/vs/base/browser/ui/codicons/codicon/codicon.ttf", "./assets/monaco/min/vs/base/browser/ui/codicons/codicon/codicon.ttf", http.FS(staticFiles))
 	handler.StaticFileFS("/assets/monaco/min/vs/basic-languages/xml/xml.js", "./assets/monaco/min/vs/basic-languages/xml/xml.js", http.FS(staticFiles))
+
+	langs := []string{"en", "fr", "de", "ar", "es", "fi", "he", "it", "ja", "nl", "ru", "sv"}
+	for _, lang := range langs {
+		relativePath := "/assets/i18n/" + lang + ".json"
+		filePath := "." + relativePath
+		handler.StaticFileFS(relativePath, filePath, http.FS(staticFiles))
+	}
 
 	// Swagger
 	swaggerHandler := ginSwagger.DisablingWrapHandler(swaggerFiles.Handler, "DISABLE_SWAGGER_HTTP_HANDLER")
